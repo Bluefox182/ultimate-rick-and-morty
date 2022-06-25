5de00ed1915ae09f5de20b539bdbd2e88058b9f1
bluefox182 <fergaldoss@gmail.com>
2022-06-24 23:54:20 -0500

    remove Link import from App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
 import Switch from '@mui/material/Switch';
